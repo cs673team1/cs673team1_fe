2b2cd6f625e5b67565a97d9e9dcb9cd75f074b6a
Lynn Cistulli <lynncistulli@gmail.com>
2018-11-14 00:51:43 -0500

    Commit additions to card.php to get and add cards

---
+++ src/php/cardType.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: lynnc
+ * Date: 11/14/2018
+ * Time: 12:49 AM
+ */
+require_once('dB.php');
+
+class cardType
+{
+    // The database interface
+    protected static $dbInterface;
+
+    function __construct($db)
+    {
+        // Try and connect to the database interface
+        if (!isset(self::$dbInterface)) {
+            Self::$dbInterface = $db;
+        }
+
+        // If database interface was not successful, handle the error
+        if (self::$dbInterface === false) {
+            // Handle error - notify administrator, log to a file, show an error screen, etc.
+            return false;
+        }
+        return self::$dbInterface;
+    }
+
+    /**
+     * Get all cardTypes from database
+     *
+     * @return mixed The result of the mysqli::query() function
+     */
+    public function getAllCardTypes()
+    {
+        $result = self::$dbInterface -> query("SELECT typeID, typeName FROM cardType");
+        return $result;
+    }
+
+}
---
+++ src/php/status.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: lynnc
+ * Date: 11/14/2018
+ * Time: 12:41 AM
+ */
+require_once('dB.php');
+
+class status
+{
+    // The database interface
+    protected static $dbInterface;
+
+    function __construct($db)
+    {
+        // Try and connect to the database interface
+        if (!isset(self::$dbInterface)) {
+            Self::$dbInterface = $db;
+        }
+
+        // If database interface was not successful, handle the error
+        if (self::$dbInterface === false) {
+            // Handle error - notify administrator, log to a file, show an error screen, etc.
+            return false;
+        }
+        return self::$dbInterface;
+    }
+
+    /**
+     * Get all status values from database
+     *
+     * @return mixed The result of the mysqli::query() function
+     */
+    public function getAllStatus()
+    {
+        $result = self::$dbInterface -> query("SELECT statusID, statusName FROM status");
+        return $result;
+    }
+
+}
