ccad0a163f0773d2b9c619c7d92cc7253a3c8132
Lynn Cistulli <lynncistulli@gmail.com>
2018-11-17 15:52:53 -0500

    Added function to cardType class to check if a cardTypeID exists in the database

--- src/php/cardType.php
+++ src/php/cardType.php
@@ -38,4 +38,20 @@ class cardType
         return $result;
     }
 
+    /**
+     * Check if cardTypeID exists in database
+     *
+     * @param $cardTypeID the cardTypeID
+     * @return boolean 1 if cardTypeID exists 0 if it doesn't exist
+     */
+    public function exists($cardTypeID)
+    {
+        $result = self::$dbInterface->query("SELECT typeID FROM cardType WHERE typeID = '" . $cardTypeID . "'");
+        if ($result->num_rows > 0) {
+            return 1;
+        } else {
+            return 0;
+        }
+    }
+
 }
