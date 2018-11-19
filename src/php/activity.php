f6253f032fba761173d36c275ef768bf6d0dd169
Lynn Cistulli <lynncistulli@gmail.com>
2018-11-18 21:28:40 -0500

    Added activity class

---
+++ src/php/activity.php
@@ -0,0 +1,113 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: lynnc
+ * Date: 11/18/2018
+ * Time: 9:23 PM
+ */
+
+require_once('dB.php');
+
+class activity
+{
+    // The database interface
+    protected static $dbInterface;
+
+    function __construct($db)
+    {
+        // Try and connect to the database interface
+        if (!isset(self::$dbInterface)) {
+            Self::$dbInterface = $db;
+        }
+
+        // If database interface was not successful, handle the error
+        if (self::$dbInterface === false) {
+            // Handle error - notify administrator, log to a file, show an error screen, etc.
+            return false;
+        }
+        return self::$dbInterface;
+    }
+
+    /**
+     * Get all activity from database (activityID, content, time, user_userID, card_cardID)
+     *
+     * @return mixed The result of the mysqli::query() function
+     */
+    public function getAllActivity()
+    {
+        $result = self::$dbInterface -> query("SELECT activityID, content, time, user_userID, card_cardID 
+              FROM activity");
+        return $result;
+    }
+
+    /**
+     * Get activity by activityID from database
+     *
+     * @param activityID The activityID
+     * @return mixed The result of the mysqli::query() function
+     */
+    public function getActivityByID($activityID)
+    {
+        $result = self::$dbInterface -> query("SELECT content, time, user_userID, card_cardID 
+              FROM activity WHERE activityID='$activityID'");
+        return $result;
+    }
+
+    /**
+     * Get check an activityID is in database
+     *
+     * @param $activityID the activityID
+     * @return mixed The result of the mysqli::query() function
+     */
+    public function exists($activityID)
+    {
+        $result = self::$dbInterface->query("SELECT activityID FROM activity WHERE activityID = '" . $activityID . "'");
+        if ($result->num_rows > 0) {
+            return 1;
+        } else {
+            return 0;
+        }
+    }
+
+    /**
+     * Get activity for a specific userID
+     *
+     * @param userID The userID
+     * @return mixed The result of the mysqli::query() function
+     */
+    public function getActivityForUser($userID)
+    {
+        $result = self::$dbInterface -> query("SELECT activityID, content, time, user_userID, card_cardID 
+              FROM activity WHERE user_userID='$userID'");
+        return $result;
+    }
+
+    /**
+     * Get activity for a specific cardID
+     *
+     * @param cardID The cardID
+     * @return mixed The result of the mysqli::query() function
+     */
+    public function getActivityForCard($cardID)
+    {
+        $result = self::$dbInterface -> query("SELECT activityID, content, time, user_userID, card_cardID 
+              FROM activity WHERE card_cardID='$cardID'");
+        return $result;
+    }
+
+    /**
+     * Add activity to specific list
+     *
+     * @param $content
+     * @param $userID
+     * @param $cardID
+     * @return mixed The result of the mysqli::query() function
+     */
+    public function addActivity($content, $userID, $cardID)
+    {
+        $result = self::$dbInterface -> query("INSERT INTO activity (activityID, content, time, user_userID, card_cardID) 
+                                                VALUES (DEFAULT, '".$content."',  now(), '".$userID."',
+                                                '".$cardID."')");
+        return $result;
+    }
+}
