fbcb58b55cf96c9ef2a1d54c1780adbfc842d1ea
Jon Shapiro <Jon_Shapiro@local>
2018-11-20 20:12:24 -0500

    try to add in card-id, later make it uneditable ... for now do not change

--- src/js/cards.js
+++ src/js/cards.js
@@ -17,27 +17,28 @@ function getCards(list, tag)
          for (x in data)
          {
             cards +=
-               "<!-- Story Card -->\n" +
-               "<div class=\"card\" style=\"width: auto;\">\n" +
-               "   <div class=\"card-body\">\n" +
-               "      <h5 class=\"card-title\">" + data[x].cardName + " <i>(" + data[x].status + ")</i></h5>\n" +
-               "      <h6 class=\"card-subtitle mb-2 text-muted\">Owner</h6>\n" +
-               "      <p class=\"card-text\">" + data[x].description + "</p>\n" +
-               "      <div class=\"btn-group\">\n" +
-               "         <button type=\"button\" class=\"btn btn-primary dropdown-toggle\" data-toggle=\"dropdown\">\n" +
-               "            Actions\n" +
-               "            <span class=\"caret\"></span>\n" +
-               "         </button>\n" +
-               "         <ul class=\"dropdown-menu\" role=\"menu\">\n" +
-               "            <li><a href=\"#\">Move</a></li>\n" +
-               "            <li><a href=\"#\">Edit</a></li>\n" +
-               "            <li class=\"divider\"></li>\n" +
-               "            <li><a type=\"button\" data-toggle=\"modal\" data-target=\"#EditModal\">Delete</a></li>\n" +
-               "         </ul>\n" +
-               "      </div>\n" +
-               "   </div>\n" +
-               "</div> <!--End Story Card -->\n" +
-               "<br>";
+                "<!-- Story Card -->\n" +
+                "<div class=\"card\" style=\"width: auto;\">\n" +
+                "   <div class=\"card-body\">\n" +
+                "      <h7 class=\"card-id\">" + data[x].cardID +"</h7>\n" +
+                "      <h5 class=\"card-title\">" + data[x].cardName + " <i>(" + data[x].status + ")</i></h5>\n" +
+                "      <h6 class=\"card-subtitle mb-2 text-muted\">Owner</h6>\n" +
+                "      <p class=\"card-text\">" + data[x].description + "</p>\n" +
+                "      <div class=\"btn-group\">\n" +
+                "         <button type=\"button\" class=\"btn btn-primary dropdown-toggle\" data-toggle=\"dropdown\">\n" +
+                "            Actions\n" +
+                "            <span class=\"caret\"></span>\n" +
+                "         </button>\n" +
+                "         <ul class=\"dropdown-menu\" role=\"menu\">\n" +
+                "            <li><a href=\"#\">Move</a></li>\n" +
+                "            <li><a href=\"#\">Edit</a></li>\n" +
+                "            <li class=\"divider\"></li>\n" +
+                "            <li><a type=\"button\" data-toggle=\"modal\" data-target=\"#EditModal\">Delete</a></li>\n" +
+                "         </ul>\n" +
+                "      </div>\n" +
+                "   </div>\n" +
+                "</div> <!--End Story Card -->\n" +
+                "<br>";
          }
 
          document.getElementById(tag).innerHTML = cards;
