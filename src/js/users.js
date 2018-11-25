c37922481f1fba061de0156c964b36eccd083733
Allen <abouch@bu.edu>
2018-11-25 11:11:06 -0500

    Owner fields are now drop-downs that are populated with users when page loads.

--- src/js/users.js
+++ src/js/users.js
@@ -12,14 +12,20 @@ function getUsers()
       if (this.readyState == 4 && this.status == 200)
       {
          const data = JSON.parse(this.responseText);
+
+         // users is for the User Login; formUsers is to populate owner fields in forms.
          users =  "<option default=\"login\">User Login</option>";
+         formUsers = "<option default=\"none\">None Selected</option>";
          for (x in data)
          {
            users += "<option value=\"" + data[x].userName + "\">" + data[x].userName + "</option>";
+           formUsers +=  "<option value=\"" + data[x].userName + "\">" + data[x].userName + "</option>";
          }
 
          document.getElementById("user-list").innerHTML = users;
-         //
+         document.forms["newStoryForm"].elements["newStoryOwner"].innerHTML = formUsers;
+         document.forms["newBugForm"].elements["newBugOwner"].innerHTML = formUsers;
+         document.forms["editCardForm"].elements["editCardOwner"].innerHTML = formUsers;
       }
    };
 
