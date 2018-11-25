35e6884d805f9ee3a517592681e1c2f55a10038f
Jon Shapiro <Jon_Shapiro@local>
2018-11-25 15:44:12 -0500

    check all inputs for null using long form ...

--- src/php/newBug.php
+++ src/php/newBug.php
@@ -68,5 +68,6 @@ if ($statusResult->num_rows > 0) {
 }
 
 if ($cardName != null and $typeID != null and $description != null and $statusID != null and $complexity != null and $listID != null) {
-    $result = $card->addCardToList($cardName, $typeID, $description, $statusID, $complexity, $listID);
+    $result = $card->addCardToList("DELETE", $typeID, "FAKE BUG 2", 1, 4, $listID);
+    //$result = $card->addCardToList($cardName, $typeID, $description, $statusID, $complexity, $listID);
 }
