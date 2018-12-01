fc81902ba528b81abb36d34de453a77864b7ac8b
Jon Shapiro <Jon_Shapiro@local>
2018-12-01 18:53:49 -0500

    do not match "" as this matches everything (seemingly)

--- src/js/new_bug.js
+++ src/js/new_bug.js
@@ -8,7 +8,7 @@ $(document).ready(function () {
         var status = document.querySelector('input[name=newBugStatusBtnGrp]:checked').value;
         var user = document.getElementById("user-list").value;
 
-        if (!user || user.toString().match("") || user.toString().match(/login/i)) {
+        if (!user || user.toString().match(/login/i)) {
             alert("Please log in");
             location.assign(document.getElementById("homeURL"));
             location.reload(true);
