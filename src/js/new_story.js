bcb08f3143535f781cf82e2d16859783eea4fe9b
Jon Shapiro <Jon_Shapiro@local>
2018-12-01 22:17:07 -0500

    make newStory handle dialogs like newBug

--- src/js/new_story.js
+++ src/js/new_story.js
@@ -1,5 +1,26 @@
 // newStory java script, invoked from newStory modal ...
 $(document).ready(function () {
+    function dataValid() {
+        var title = $("#newBugTitle").val();
+        var desc = $("#newStoryDesc").val();
+        var user = document.getElementById("user-list").value;
+
+        if (!user || user.toString().match(/login/i)) {
+            document.getElementById("newStoryError").hidden = '';
+            document.getElementById("newStoryError").innerHTML = "Please log in";
+            $("#newStoryModal").reload(true);
+            return false;
+        }
+        else if (!(title && desc)) {
+            document.getElementById("newStoryError").hidden = '';
+            document.getElementById("newStoryError").innerHTML = "Please fill in title and description";
+            $("#newStoryModal").reload(true);
+            return false;
+        }
+
+        return true;
+    }
+
     $("#newStoryForm").on("submit", function(e) {
         var postData = $(this).serializeArray();
         var title = $("#newStoryTitle").val();
@@ -8,36 +29,23 @@ $(document).ready(function () {
         var status = document.querySelector('input[name=newStoryStatusBtnGrp]:checked').value;
         var user = document.getElementById("user-list").value;
 
-        if (!user || user.toString().match(/login/i)) {
-            alert("Please log in");
-            location.assign(document.getElementById("homeURL"));
-            location.reload(true);
-        }
-        else if (!(title && desc)) {
-            alert("Please fill in title and description");
-            location.assign(document.getElementById("homeURL"));
-            location.reload(true);
-        }
-        else {
-            var postData = 'Title=' + title + '&Owner=' + owner + '&Description=' + desc + '&Status=' + status + '&UserName=' + user;
-            var formURL = document.getElementById("homeURL") + "/" + $(this).attr("action");
-            $.ajax({
-                url: formURL,
-                type: "POST",
-                data: postData,
-                success: function (data, textStatus, jqXHR) {
-                    $('#newStoryForm .modal-header .modal-title').html("Added new Story");
-                    $('#newStoryForm .modal-body').html(data);
-                    $("#newStorySubmit").remove();
-                    $("#newStorySubmit").reset(); // clear old data
-                    location.reload(true);
-                },
-                error: function (jqXHR, status, error) {
-                    console.log(status + ": " + error);
-                }
-            });
-            e.preventDefault();
-        }
+        var postData = 'Title=' + title + '&Owner=' + owner + '&Description=' + desc + '&Status=' + status + '&UserName=' + user;
+        var formURL = document.getElementById("homeURL") + "/" + $(this).attr("action");
+        $.ajax({
+            url: formURL,
+            type: "POST",
+            data: postData,
+            success: function (data, textStatus, jqXHR) {
+                //$('#newStoryForm .modal-header .modal-title').html("Added new Story");
+                //$('#newStoryForm .modal-body').html(data);
+                $("#newStorySubmit").remove();
+                $("#newStorySubmit").reset(); // clear old data
+            },
+            error: function (jqXHR, status, error) {
+                console.log(status + ": " + error);
+            }
+        });
+        e.preventDefault();
     });
 
     $("#newStorySubmit").on('click', function() {
