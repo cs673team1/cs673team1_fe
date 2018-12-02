f5068cbc53ac9ac2bc0626564d7cbd3419eed9e4
Jon Shapiro <Jon_Shapiro@local>
2018-12-01 19:37:56 -0500

    try separating logic ...

--- src/js/new_bug.js
+++ src/js/new_bug.js
@@ -1,4 +1,24 @@
 // newBug java script, invoked from newBug modal ...
+function dataValid() {
+    var title = $("#newBugTitle").val();
+    var desc = $("#newBugDesc").val();
+    var user = document.getElementById("user-list").value;
+
+    if (!user || user.toString().match(/login/i)) {
+        document.getElementById("newBugError").innerHTML = "Please log in";
+        return false;
+        //location.assign(document.getElementById("homeURL"));
+        //location.reload(true);
+    }
+    else if (!(title && desc)) {
+        document.getElementById("newBugError").innerHTML = "Please fill in title and description";
+        return false;
+        //location.assign(document.getElementById("homeURL"));
+        //location.reload(true);
+    }
+    return true;
+}
+
 $(document).ready(function () {
     $("#newBugForm").on("submit", function(e) {
         var postData = $(this).serializeArray();
@@ -8,39 +28,29 @@ $(document).ready(function () {
         var status = document.querySelector('input[name=newBugStatusBtnGrp]:checked').value;
         var user = document.getElementById("user-list").value;
 
-        if (!user || user.toString().match(/login/i)) {
-            document.getElementById("newBugError").innerHTML = "Please log in";
-            //location.assign(document.getElementById("homeURL"));
-            //location.reload(true);
-        }
-        else if (!(title && desc)) {
-            document.getElementById("newBugError").innerHTML = "Please fill in title and description";
-            //location.assign(document.getElementById("homeURL"));
-            //location.reload(true);
-        }
-        else {
-            var postData = 'Title=' + title + '&Owner=' + owner + '&Description=' + desc + '&Status=' + status + '&UserName=' + user;
-            var formURL = document.getElementById("homeURL") + "/php/newBug.php";
-            $.ajax({
-                url: formURL,
-                type: "POST",
-                data: postData,
-                success: function (data, textStatus, jqXHR) {
-                    $('#newBugForm .modal-header .modal-title').html("New bug added");
-                    $('#newBugForm .modal-body').html(data);
-                    $("#newBugSubmit").remove();
-                    $("#newBugSubmit").reset(); // clear old data
-                    location.reload(true);
-                },
-                error: function (jqXHR, status, error) {
-                    console.log(status + ": " + error);
-                }
-            });
-            e.preventDefault();
-        }
+        var postData = 'Title=' + title + '&Owner=' + owner + '&Description=' + desc + '&Status=' + status + '&UserName=' + user;
+        var formURL = document.getElementById("homeURL") + "/php/newBug.php";
+        $.ajax({
+            url: formURL,
+            type: "POST",
+            data: postData,
+            success: function (data, textStatus, jqXHR) {
+                $('#newBugForm .modal-header .modal-title').html("New bug added");
+                $('#newBugForm .modal-body').html(data);
+                $("#newBugSubmit").remove();
+                $("#newBugSubmit").reset(); // clear old data
+                location.reload(true);
+            },
+            error: function (jqXHR, status, error) {
+                console.log(status + ": " + error);
+            }
+        });
+        e.preventDefault();
     });
 
     $("#newBugSubmit").on('click', function() {
-        $("#newBugForm").submit();
+        if (dataValid()) {
+            $("#newBugForm").submit();
+        }
     });
 });
