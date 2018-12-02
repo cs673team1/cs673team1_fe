595d9a0324e4eb896ba059e70efb111348dc0b82
Jon Shapiro <Jon_Shapiro@local>
2018-12-01 21:15:11 -0500

    try using new dataValid for editCard

--- src/js/edit_card.js
+++ src/js/edit_card.js
@@ -30,37 +30,29 @@ $(document).ready(function () {
         var status = document.querySelector('input[name=editCardStatusBtnGrp]:checked').value;
         var user = document.getElementById("user-list").value;
 
-        if (!user || user.toString().match(/login/i)) {
-            alert("Please log in");
-            location.reload(true);
-        }
-        else if (!(title && desc)) {
-            alert("Please fill in title and description");
-            location.reload(true);
-        }
-        else {
-            var postData = 'CardID=' + cardID + '&Title=' + title + '&Owner=' + owner + '&Description=' + desc + '&Status=' + status + '&UserName=' + user;
-            var formURL = document.getElementById("homeURL") + "/" + $(this).attr("action");
-            $.ajax({
-                url: formURL,
-                type: "POST",
-                data: postData,
-                success: function (data, textStatus, jqXHR) {
-                    $('#editCardForm .modal-header .modal-title').html("Edited card");
-                    $('#editCardForm .modal-body').html(data);
-                    $("#editCardSubmit").remove();
-                    // do not reset data ... maybe user wants to keep on editing
-                    location.reload(true);
-                },
-                error: function (jqXHR, status, error) {
-                    console.log(status + ": " + error);
-                }
-            });
-            e.preventDefault();
-        }
+        var postData = 'CardID=' + cardID + '&Title=' + title + '&Owner=' + owner + '&Description=' + desc + '&Status=' + status + '&UserName=' + user;
+        var formURL = document.getElementById("homeURL") + "/" + $(this).attr("action");
+        $.ajax({
+            url: formURL,
+            type: "POST",
+            data: postData,
+            success: function (data, textStatus, jqXHR) {
+                $('#editCardForm .modal-header .modal-title').html("Edited card");
+                $('#editCardForm .modal-body').html(data);
+                $("#editCardSubmit").remove();
+                // do not reset data ... maybe user wants to keep on editing
+                location.reload(true);
+            },
+            error: function (jqXHR, status, error) {
+                console.log(status + ": " + error);
+            }
+        });
+        e.preventDefault();
     });
 
     $("#editCardSubmit").on('click', function() {
-        $("#editCardForm").submit();
+        if (dataValid()) {
+            $("#editCardForm").submit();
+        }
     });
 });
