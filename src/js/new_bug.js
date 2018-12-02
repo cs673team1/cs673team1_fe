c667996e72198b0a473c60755450a23b62b0b243
Jon Shapiro <Jon_Shapiro@local>
2018-12-01 19:56:23 -0500

    nope ... does not work either ...

--- src/js/new_bug.js
+++ src/js/new_bug.js
@@ -19,42 +19,39 @@ function dataValid() {
     return true;
 }
 
-//$(document).ready(function () {
-//    $("#newBugForm").on("submit", function(e) {
-function newBugSubmitInner() {
-    var postData = $(this).serializeArray();
-    var title = $("#newBugTitle").val();
-    var owner = $("#newBugOwner").val();
-    var desc = $("#newBugDesc").val();
-    var status = document.querySelector('input[name=newBugStatusBtnGrp]:checked').value;
-    var user = document.getElementById("user-list").value;
+$(document).ready(function () {
+    $("#newBugForm").on("submit", function (e) {
+        var postData = $(this).serializeArray();
+        var title = $("#newBugTitle").val();
+        var owner = $("#newBugOwner").val();
+        var desc = $("#newBugDesc").val();
+        var status = document.querySelector('input[name=newBugStatusBtnGrp]:checked').value;
+        var user = document.getElementById("user-list").value;
 
-    var postData = 'Title=' + title + '&Owner=' + owner + '&Description=' + desc + '&Status=' + status + '&UserName=' + user;
-    var formURL = document.getElementById("homeURL") + "/php/newBug.php";
-    $.ajax({
-        url: formURL,
-        type: "POST",
-        data: postData,
-        success: function (data, textStatus, jqXHR) {
-            $('#newBugForm .modal-header .modal-title').html("New bug added");
-            $('#newBugForm .modal-body').html(data);
-            $("#newBugSubmit").remove();
-            $("#newBugSubmit").reset(); // clear old data
-        },
-        error: function (jqXHR, status, error) {
-            console.log(status + ": " + error);
-        }
+        var postData = 'Title=' + title + '&Owner=' + owner + '&Description=' + desc + '&Status=' + status + '&UserName=' + user;
+        var formURL = document.getElementById("homeURL") + "/php/newBug.php";
+        $.ajax({
+            url: formURL,
+            type: "POST",
+            data: postData,
+            success: function (data, textStatus, jqXHR) {
+                $('#newBugForm .modal-header .modal-title').html("New bug added");
+                $('#newBugForm .modal-body').html(data);
+                $("#newBugSubmit").remove();
+                $("#newBugSubmit").reset(); // clear old data
+            },
+            error: function (jqXHR, status, error) {
+                console.log(status + ": " + error);
+            }
+        });
+        e.preventDefault();
     });
-    //e.preventDefault();
-    //});
-}
 
     $("#newBugSubmit").on('click', function() {
         if (dataValid()) {
-            //$("#newBugForm").submit();
-            newBugSubmitInner();
+            $("#newBugForm").submit();
             $("#newBugModal").hide();
             location.reload(true);
         }
     });
-//});
+});
